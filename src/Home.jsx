7d5bdfb594d96dcaa11286831e1fb1950f869cac
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-11-25 11:45:45 -0300

    Update 2
    Navbar lateral feito, porém não estilizado. Importei alguns ícones do Bootstrap e estou colocando fonte personalizada no site.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,30 @@
+import "../src/CSS/style.css"
 export default function Home(){
     return(
         <nav class="MenuLateral">
             <div class="BotaoDeHamburguer">
-                
+              <i class="bi bi-list"></i>
             </div>
+            <ul>
+              <li class="ItemMenu">
+                <a href="#">
+                    <span class="icone"><i class="bi bi-house"></i></span>
+                    <span class="link">Menu Inicial</span>
+                </a>
+              </li>
+              <li class="ItemMenu">
+                <a href="#">
+                    <span class="icone"><i class="bi bi-graph-up-arrow"></i></span>
+                    <span class="link">Destaque</span>
+                </a>
+              </li>
+              <li class="ItemMenu">
+                <a href="#">
+                    <span class="icone"><i class="bi bi-download"></i></span>
+                    <span class="link">Enviar Vídeo</span>
+                </a>
+              </li>             
+            </ul>
         </nav>
      );
 }
