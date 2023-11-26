58d7e13df6be319ec04c38f8f406a4179aaa6dd1
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-11-25 21:07:05 -0300

    Update 9
    Foi feito o card, foi adicionado uma logo e um nome para o site e houve uma mudança de cores

--- src/Componentes/ListaDeVideos.jsx
+++ src/Componentes/ListaDeVideos.jsx
@@ -1,16 +1,64 @@
 export default function Videos(){
     return(
         <div class="conteinerDeCards">
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            
-        </div>
+    
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>  
+            <div class="card">
+              <div class="conteudo">
+                <img src="arongas.jpg"></img>
+                <h3>Carangueijo assassino (muito incrível)</h3>
+                <p>Canal Do Jorginho 123 <i class="bi bi-badge-8k-fill"></i></p>
+              </div>
+            </div>   
+
+        </div>    
     );
 }
