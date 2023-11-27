5f2336a188f74b9184420c00263fa9165182670a
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-11-27 17:31:05 -0300

    Update 11
    Fazendo uma função que vai apagar todos os elementos de uma classe, sendo essa a classe "itemMenu".

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+var menuItem = document.querySelectorAll('.itemMenu')
+
+//foreach serve para fazer uma varredura no array
+function selecionarLink(){
+    menuItem.forEach((item) => 
+    //vai remover dinamicamente uma classe de um item
+    item.classList.remove
+    )
+}
