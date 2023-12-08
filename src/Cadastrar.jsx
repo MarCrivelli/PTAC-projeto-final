9e297585817cf2ee8f7f9bcaf6ee42767fa91376
MarCrivelli <131816938+MarCrivelli@users.noreply.github.com>
2023-12-08 17:54:30 -0400

    Update 20
    Foi iniciado um processo para fazer surgir uma página de cadastro, juntamente com um card com as informações

--- src/Cadastrar.jsx
+++ src/Cadastrar.jsx
@@ -4,13 +4,6 @@ import Nav from './Componentes/Nav';
 import "../src/CSS/style.css";
 import "../src/menu.js";
 export default function CadastrarVideos(){
- 
-    const localStorageLista = JSON.parse(localStorage.getItem("Lista")) || [];
-    //useState define um estado para a lista
-    const [videos, setVideo] = useState("")
-    const [titulo, setTitulo] = useState("")
-    const [descricao, setDescricao] = useState("")
-
     return(
          <div>
             <Nav/>
--- src/Componentes/Videos.jsx
+++ src/Componentes/Videos.jsx
@@ -1,21 +1,36 @@
 import CadastrarVideos from "../Cadastrar";
+import {useState} from "react";
 export default function Videos({vid}){
     //const listaVideos = JSON.parse(localStorage.getItem('list')) || [];
+
+    const localStorageLista = JSON.parse(localStorage.getItem("Lista")) || [];
+    //useState define um estado para a lista
+    const [videos, setVideo] = useState();
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+
+    const salvar = () => {
+      alert ("salvar");
+  }
+  const remover = () => {
+      alert ("remover");
+  }
     return(
       <div>
-        <CadastrarVideos/>
+        <CadastrarVideos/>  
         <div class="conteinerDeCards">
-          
             <div class="container">
               <div class="card">
                 <div class="conteudo">
-                  <img ></img>
+                  <iframe value={videos} onChange={(e) => setVideo(e.target.value)}></iframe>
                   <h3></h3>
                   <p><i class="bi bi-badge-8k-fill"></i></p>
                 </div>
+                <button onChange={salvar}><a>Salvar</a></button>
+                <button onChange={remover}><a>Remover</a></button>
               </div>    
             </div>
-        </div>   
+          </div> 
       </div>
         
     );
